7967fa611146bb821e54769a94a776228ed80781
WelmoedJonas <moedsus@hotmail.com>
2022-08-25 10:56:06 +0200

    opdracht2.1

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -12,7 +12,9 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + "je");
+}
 
 
 // ==========================================
