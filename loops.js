b5e2fd764e39adff1519abcdab70f312a4449902
WelmoedJonas <moedsus@hotmail.com>
2022-08-25 08:59:54 +0200

    huiswerk-loops

--- loops.js
+++ loops.js
@@ -6,8 +6,6 @@
 // Hoera!
 // ==========================================
 
-
-
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
 // Verwachte uitkomsten:
